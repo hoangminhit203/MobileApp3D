dc2aa154616c7fdce64988153413fb3a803f82c1
Tiến Nguyễn <115931736+TienNguyenHoang@users.noreply.github.com>
2025-09-09 18:09:04 +0700

    added detail page

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,17 @@
 import { ScrollView, Text } from "react-native";
-import { ForUContainer } from "../components";
+
+import { ForUContainer } from "@/components";
+import seed from "@/interfaces/seedData";
 
 export default function App() {
-  const seed = [
-    {
-      id: "1",
-      title: "Modern",
-      category: "Perfect for dining rooms",
-      code: "A-DIY",
-      imageUrl: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-      id: "2",
-      title: "Wooden Chair",
-      category: "Perfect for dining rooms",
-      code: "A-DIY",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0Oc9tGIzrpArxdS1fwqz1vI8jrVMefimow&s",
-    },
-    {
-      id: "3",
-      title: "Coffee Table",
-      category: "Compact and stylish",
-      code: "A-DIY", 
-      imageUrl: "https://reactjs.org/logo-og.png",
-    },
-  ];
+  
 
   return (
     <ScrollView className="flex-1 bg-gray-100 p-4 mb-10">
       <Text className="uppercase text-2xl font-extrabold mb-10">For You</Text>
-
-      <ForUContainer sectionTitle="Now Popular in Furniture" items={seed} />
-      <ForUContainer sectionTitle="Now Popular in Electronics" items={seed} />
-      <ForUContainer sectionTitle="Now Popular in sports" items={seed} />
+        {seed.map((item, index) => (
+            <ForUContainer key={index} sectionTitle={item.sectionTitle} route={item.route} items={item.data} />
+        ))}
     </ScrollView>
   );
 }
---
+++ interfaces/seedData.ts
@@ -0,0 +1,225 @@
+const seed = [
+    {
+        sectionTitle: 'Now Popular in Furniture',
+        route: 'furniture',
+        data: [
+            {
+                id: "furniture-1",
+                title: "Modern",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3furniture-wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "furniture-2",
+                title: "Wooden Chair",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0Oc9tGIzrpArxdS1fwqz1vI8jrVMefimow&s",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "furniture-3",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "furniture-4",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "furniture-5",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "furniture-6",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+        ]
+
+    },
+    {
+        sectionTitle: 'Now Popular in Electronics',
+        route: 'electronics',
+        data: [
+            {
+                id: "electronics-1",
+                title: "Modern",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3electronics-wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "electronics-2",
+                title: "Wooden Chair",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0Oc9tGIzrpArxdS1fwqz1vI8jrVMefimow&s",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "electronics-3",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "electronics-4",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "electronics-5",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "electronics-6",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+        ]
+
+    },
+    {
+        sectionTitle: 'Now Popular in Sports',
+        route: 'sports',
+        data: [
+            {
+                id: "sports-1",
+                title: "Modern",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3sports-wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "sports-2",
+                title: "Wooden Chair",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0Oc9tGIzrpArxdS1fwqz1vI8jrVMefimow&s",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "sports-3",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "sports-4",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "sports-5",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "sports-6",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+        ]
+
+    },
+    {
+        sectionTitle: 'Now Popular in Toys',
+        route: 'toys',
+        data: [
+            {
+                id: "toys-1",
+                title: "Modern",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3toys-wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "toys-2",
+                title: "Wooden Chair",
+                category: "Perfect for dining rooms",
+                code: "A-DIY",
+                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0Oc9tGIzrpArxdS1fwqz1vI8jrVMefimow&s",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'
+            },
+            {
+                id: "toys-3",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "toys-4",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "toys-5",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+            {
+                id: "toys-6",
+                title: "Coffee Table",
+                category: "Compact and stylish",
+                code: "A-DIY", 
+                imageUrl: "https://reactjs.org/logo-og.png",
+                description: 'ktisfdsfkdsflskdafjkdsfj;dslkj;ákfjsdafjsdkafjsdakfsdjlfkdsjfkasjfkdsjafasdfafdsfsd'},
+
+        ]
+
+    },
+
+];
+
+export default seed
