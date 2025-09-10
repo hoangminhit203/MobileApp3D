9f6b0ac536a1139e911253d6db6f5986e17b16c0
Tiến Nguyễn <115931736+TienNguyenHoang@users.noreply.github.com>
2025-09-10 11:22:36 +0700

    update for you UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,44 @@
 import FoUContainer from "@/components/FoUContainer";
 import { images } from "@/constants/images";
 import seed from "@/interfaces/seedData";
-import { Image, ScrollView, View } from "react-native";
+import { useState } from "react";
+import { Image, ImageBackground, ScrollView, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  // State xác định sticky header
+  const [isSticky, setIsSticky] = useState(false);
+ 
+  const handleScroll = (event: any) => {
+    const y = event.nativeEvent.contentOffset.y;
+    setIsSticky(y > 80); // khi cuộn xuống quá 80px thì stick header
+  };
   return (
     // Đây là trang for you, hiển thị các sản phẩm được đề xuất
-    <View className="flex-1 bg-white">
+    <SafeAreaView className="flex-1" edges={["left", "right",]}>
       {/* Background */}
       <Image source={images.bg} className="absolute w-full h-full" />
+      {/* Header */}
+      <ImageBackground
+        source={isSticky ? images.bg : null} // hình khi sticky
+        className={`w-full ${
+          isSticky ? "absolute top-0 left-0 right-0 z-10 pt-10" : "relative pt-20"
+        }`}
+        resizeMode="cover"
+      >
+        <Text
+          className={`uppercase font-extrabold px-4 ${
+            isSticky ? "text-2xl text-center py-3" : "text-4xl"
+          }`}
+        >
+          For You
+        </Text>
+      </ImageBackground>
 
       <ScrollView
-        className="flex-1 px-4"
+        className="px-4"
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 20, paddingTop: 60 }}
       >
@@ -25,6 +52,6 @@ export default function Index() {
           />
         ))}
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
